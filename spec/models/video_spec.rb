d0c17653ef7e7ef468cab314ff00065d99e57de0
Hector Otero <otero111@hotmail.com>
2015-07-14 00:37:09 -0700

    added tests for video and category associations

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,4 +6,10 @@ describe Video do
     video.save
     expect(Video.first).to eq(video) 
   end
+
+  it "belongs to a category" do
+    dramas = Category.create(name: dramas)
+    monk = Video.create(title: "Monk", desciption: "detective mind reader", category: dramas)
+    expect(monk.category).to eq(dramas)
+  end
 end
