8183da9d2790857aaf0df4b182379d9a8018a10e
Hector Otero <otero111@hotmail.com>
2015-07-13 22:33:00 -0700

    added two simple tests

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Category do
+  it "saves itself" do
+    category = Category.new(name: "comedies")
+    category.save
+    expect(Category.first).to eq(category)
+  end
+end
