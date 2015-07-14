d41ccb8e42340ebd7ce4b8662cde5eb9f63c118a
Hector Otero <otero111@hotmail.com>
2015-07-13 18:20:03 -0700

    added first test successfully

--- db/migrate/20150605033350_create_videos.rb
+++ db/migrate/20150605033350_create_videos.rb
@@ -2,7 +2,7 @@ class CreateVideos < ActiveRecord::Migration
   def change
     create_table :videos do |t|
     	t.string :title, :small_cover_url, :large_cover_url
-    	t.text :desciption
+    	t.text :description
     	t.timestamps
     end
   end
