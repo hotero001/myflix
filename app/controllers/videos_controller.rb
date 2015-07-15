9a8af9cf7f5494447b8d563d322053889cfe4436
Hector Otero <otero111@hotmail.com>
2015-07-14 19:41:38 -0700

    added search tool

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,10 +1,15 @@
-class ApplicationController < ActionController::Base
+class VideosController < ActionController::Base
 	def index
 		@videos = Video.all
 		@categories = Category.all
 	end
 
 	def show
-		@video = Video.first
+		@video = Video.find(params[:id])
+	end
+
+	def search
+		@results = Video.search_by_title(params[:search_term])
 	end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Myflix::Application.routes.draw do
-  root to: 'ui#index'
+  root to: 'videos#index'
+  resources :videos, only: [:show] do
+  	collection do
+  	  post :search, to: "videos#search"
+  	end
+  end
   get 'ui(/:action)', controller: 'ui'
-  get 'home', to: 'videos#index'
-  get 'video', to: 'videos#show'
-  resources :categories, only: :show
+  #get 'home', to: 'videos#index'
+  #get 'video', to: 'videos#show'
+  #resources :categories, only: :show
 end
